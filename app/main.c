af66f4d829a3d1dc79cbff513a9a65cde9638770
Veronika Motina <motina@cs.petrsu.ru>
2018-07-01 15:53:11 +0300

    main.c

--- app/main.c
+++ app/main.c
@@ -1,9 +1,25 @@
-#include <stdio.h>
+/**
+ * goldbach - программа для проверки гипотезы Гольдбаха.
+ * Copyright (c) 2017, Nika Motina <motina@cs.karelia.ru>
+ * This code is licensed under a MIT-style license.
+ */
 
-#include "myfunc.h"
+#include <stdio.h>
+#include "utils.h"
 
 int main()
 {
-    printf("Hello World!\n");
-    return myfunc(2);
+    int n, m;
+    scanf("%d %d", &n, &m);
+    int i;
+    int nprimes[1001];
+
+    calculate_primes(nprimes, m);
+
+    for(i = n; i <= m; i = i + 2) {
+    int y1, x1, k;
+    k = expand(i, &x1, &y1, nprimes);
+    printf("%d %d %d %d\n", i, k, x1, y1);
+    }
+    return 0;
 }
